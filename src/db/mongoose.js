59f620892f4b4127bc946d76bc8310ecce234b7a
Muhammad <dulex51@hotmail.com>
2021-04-28 01:55:49 +0100

    major milestone achieved

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config({ path: "variables.env" });
 
 mongoose
-  .connect(process.env.LOCAL, {
+  .connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
